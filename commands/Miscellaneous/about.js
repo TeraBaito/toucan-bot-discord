66f3b5d40d7ec317c00f92436aba8c8f155ca85f
TeraBaito <lrobles2670@gmail.com>
2020-05-23 14:22:52 -0600

    errors
    should be fixed already

--- commands/Miscellaneous/about.js
+++ commands/Miscellaneous/about.js
@@ -1,26 +1,26 @@
-// const Discord = require('discord.js');
-// const botInfo = require('../../package.json');
-// const fs = require('fs');
+const Discord = require('discord.js');
+const botInfo = require('../../package.json');
+const fs = require('fs');
 
-// module.exports = { 
-//     name: 'about',
-//     helpName: 'About',
-//     category: 'Miscellaneous',
-//     aliases: ['bot-info'],
-//     usage: 'about',
-//     description: 'General Bot Information',
+module.exports = { 
+    name: 'about',
+    helpName: 'About',
+    category: 'Miscellaneous',
+    aliases: ['bot-info'],
+    usage: 'about',
+    description: 'General Bot Information',
 
-//     run: async (bot, message, args) => {
-//     let bIcon = bot.user.displayAvatarURL();
-//         let botEmbed = new Discord.MessageEmbed()
-//             .setDescription('**Bot Information**')  
-//             .setColor('#eb8334')
-//             .setThumbnail(bIcon)
-//             .addField('Bot Name', bot.user.tag)
-//             .addField('Made By', process.env.OWNER, `ID: ${process.env.OWNER_ID}`)
-//             .addField('Creation Date', bot.user.createdAt)
-//             .setFooter(`Version: ${botInfo.version}, coded with discord.js`)
+    run: async (bot, message, args) => {
+    let bIcon = bot.user.displayAvatarURL();
+        let botEmbed = new Discord.MessageEmbed()
+            .setDescription('**Bot Information**')  
+            .setColor('#eb8334')
+            .setThumbnail(bIcon)
+            .addField('Bot Name', bot.user.tag)
+            .addField('Made By', process.env.OWNER, `ID: ${process.env.OWNER_ID}`)
+            .addField('Creation Date', bot.user.createdAt)
+            .setFooter(`Version: ${botInfo.version}, coded with discord.js`)
 
-//         return message.channel.send(botEmbed);
-//     }
-// }
+        return message.channel.send(botEmbed);
+    }
+}

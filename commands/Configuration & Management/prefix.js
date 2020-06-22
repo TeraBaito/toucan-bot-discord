d59659b5c6b8d1e28fa5397ab31c230ede9d166c
TeraBaito <lrobles2670@gmail.com>
2020-06-21 19:03:18 -0600

    1.2.5

--- commands/Configuration & Management/prefix.js
+++ commands/Configuration & Management/prefix.js
@@ -14,13 +14,13 @@ module.exports = {
         if (!message.member.hasPermission('MANAGE_SERVER')) return message.channel.send('You don\'t have permission to set up a new prefix');
         if (!args[0]) return message.channel.send('Please specify a prefix');
 
-        let prefixes = JSON.parse(fs.readFileSync('./prefixes.json'), 'utf-8');
+        let prefixes = JSON.parse(fs.readFileSync('./handlers/prefixes.json'), 'utf-8');
 
         prefixes[message.guild.id] = {
             prefixes: args[0]
         };
 
-        fs.writeFile('./prefixes.json', JSON.stringify(prefixes), err => {
+        fs.writeFile('./handlers/prefixes.json', JSON.stringify(prefixes), err => {
             if(err) console.log(err);
         });
 

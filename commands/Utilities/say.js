0fc74d93a68e4bf287cd69f4b3923e1de8a386cc
TeraBaito <lrobles2670@gmail.com>
2020-07-19 19:05:26 -0600

    Toucan 1.3.4

--- commands/Utilities/say.js
+++ commands/Utilities/say.js
@@ -6,20 +6,26 @@ module.exports = {
     category: 'Utilities',
     aliases: ['echo'],
     usage: 'say (embed) ...',
-    description: 'Takes the argument and sends them back as text, then deletes the command. If the first argument is "embed", it will send an embed.',
+    description: 'Takes the argument and sends them back as text, then deletes the command. If the first argument is "embed", it will send an embed.\n**Attention:** The user needs the "Manage Messages" permission to use this command.',
 
 
     run: async (bot, message, args) => {
 
+
         if (message.deletable) message.delete();
         if (args.length < 1) return (await message.channel.send('Bruh you didn\'t even give me a message, what am I supposed to do, read your mind?'));
-        if (args[0].toLowerCase() === 'embed') {
-            const sayEmbed = new Discord.MessageEmbed()
-                .setColor('#228b22')
-                .setDescription(args.slice(1).join(' '));
-            message.channel.send(sayEmbed);
+        if (!message.member.hasPermission('MANAGE_MESSAGES')) {
+            message.channel.send('You cannot use this command, sorry, you miss the fun')
+                .then(m => m.delete({timeout: 5000}));
         } else {
-            message.channel.send(args.join(' '));
+            if (args[0].toLowerCase() === 'embed') {
+                const sayEmbed = new Discord.MessageEmbed()
+                    .setColor('#228b22')
+                    .setDescription(args.slice(1).join(' '));
+                message.channel.send(sayEmbed);
+            } else {
+                message.channel.send(args.join(' '));
+            }
         }
     }
 };
